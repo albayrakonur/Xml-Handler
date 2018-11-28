e1b29e432fb2b9e19785e4341865c57b64144ef8
Onur Albayrak <onuralbayrak@etu.edu.tr>
2018-11-28 16:34:50 +0300

    building search method-2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
@@ -15,16 +22,50 @@ import spark.template.mustache.MustacheTemplateEngine;
 public class App {
     public static ArrayList<String> search(String name,String surname) {
         System.out.println("inside search");
-        java.util.Scanner xmlreader = null;
         try {
-            xmlreader = new java.util.Scanner("EEAS.xml");
-        }catch (Exception e) {
-            System.out.println("File Cannot Read!");
-        }
-        while(xmlreader.hasNextLine()) {
-            System.out.println(xmlreader.nextLine());
-        }
-        return null;
+            File inputFile = new File("EEAS.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("ENTITY");
+            System.out.println("----------------------------");
+            
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+               Node nNode = nList.item(temp);
+               System.out.println("\nCurrent Element :" + nNode.getNodeName());
+               
+               if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                  Element eElement = (Element) nNode;
+                  System.out.println("Student roll no : " 
+                     + eElement.getAttribute("rollno"));
+                  System.out.println("First Name : " 
+                     + eElement
+                     .getElementsByTagName("firstname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Last Name : " 
+                     + eElement
+                     .getElementsByTagName("lastname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Nick Name : " 
+                     + eElement
+                     .getElementsByTagName("nickname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Marks : " 
+                     + eElement
+                     .getElementsByTagName("marks")
+                     .item(0)
+                     .getTextContent());
+               }
+            }
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+         return null;
     }
 
     public static void main(String[] args) {
